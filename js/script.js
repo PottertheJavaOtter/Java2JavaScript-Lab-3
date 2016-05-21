d116749c96767c71e1cbe7cb4610fe428791ab84
Min Lee <minlee@Zipcoder04s-MacBook-Pro.local>
2016-05-21 19:15:08 -0400

    Finished part1

--- js/script.js
+++ js/script.js
@@ -1,28 +1,57 @@
 " use strict ";
 
+var phoneEntry = [];
+phoneEntry["min"] = 222;
+phoneEntry["joe"] = 333;
+
 function PhoneBook(){
 
 };
 
 function listAllNames() {
   // code to list all names
+  var output = "";
+  for(var i in phoneEntry){
+    output+= i + "<br/>"
+  }
+  display.innerHTML = output;
 };
 
 function listAllNumbers() {
+  var output = "";
+  for(var i in phoneEntry){
+    output+= phoneEntry[i] + "<br/>"
+  }
+  display.innerHTML = output;
   // code to list all numbers
 };
+
+function listAllEntries(){
+  var output = "";
+  for(var i in phoneEntry){
+    output+= i+ "<br/>" +phoneEntry[i] + "<br/>"
+  }
+  display.innerHTML = output;
+  // code to list all entries
+}
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+  phoneEntry[name] = number;
 };
 
 function showRemove() {
   var remove = prompt("Enter name to remove");
+  delete phoneEntry[remove];
 };
 
 function showLookup() {
-  var lookup = prompt("Enter number to lookup");
+  var lookup = prompt("Enter name to lookup");
+  if(phoneEntry[lookup]!=undefined)
+    display.innerHTML = phoneEntry[lookup];
+  else
+    display.innerHTML = "That name is not found";
 };
 
 var display = document.getElementById("display");
